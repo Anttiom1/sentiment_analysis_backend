1c6b8f6bd31244c6766d68a8bb67c064baac6184
Anttiom1 <antti.mursu@edu.lapinamk.fi>
2025-02-26 15:09:42 +0200

    .

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1>Hello!!!!!</h1>"
+    return "<h1>Hello!!!asdasd!!</h1>"
 
 @app.route("/sentiment", methods=["POST"])
 def get_sentiment():
